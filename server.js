b7e75d04f48e51ed3351b1a2171c774b9d7080d7
ChummarMaly <chummar@gmail.com>
2017-04-16 11:26:01 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+ '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname+ '/public'));
+
+app.get('/',(req, res)=> {
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'You have finally arrived!.'
+  });
+  // res.send('<h1>Hello, this is Express!</h1>');
+  // res.send({
+  //   name: 'Maly',
+  //   likes: [
+  //     'Cricket','Gym','Mangoes'
+  //   ]
+  // });
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+
+    return  new Date().getFullYear();
+    //
+
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+} );
+
+app.get('/bad', (req, res) => {
+  res.send({
+    Error: "Unable to process request"
+  });
+} );
+
+
+app.listen(3000, () => {
+  console.log('Server is up and running at port 3000');
+});
